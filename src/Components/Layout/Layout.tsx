dae8a131066c6e38e8cf5cd8071f1f5816c5717e
Metin Süt <metin.sut@bordatech.com>
2022-12-10 21:57:17 +0300

    version

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -4,7 +4,9 @@ import Header from '../Header/Header';
 
 export default function Layout() {
   const {
-    location: { pathname },
+    state: {
+      currentLocation: { pathname }
+    },
     navigate
   } = useRouter();
 
